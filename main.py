7f7254e993b27c0cfa61b0058be52712feb39c3f
joaovaladao <joaovaladao161274@gmail.com>
2022-02-02 12:06:08 -0300

    Implementando funções na interface gráfica

--- main.py
+++ main.py
@@ -9,50 +9,60 @@ root.config(background = 'black')
 root.resizable(width = False, height = False)
 root.iconbitmap('assets/delivey.ico')
 
+imag_1 = PhotoImage(file= "assets/backgroundDelivery.png")
+imag_2 = PhotoImage(file = "assets/mapaBrasilia.png")
+imag_3 = PhotoImage(file = "assets/va_va_land.png")
 
+imageList = [imag_1, imag_2, imag_3]
 
 # Imagem de fundo
-imageLogo = PhotoImage(file= "assets/backgroundDelivery.png")
-background = Label(image=imageLogo)
-background.place(x=0,y=0, relwidth=1,relheight=1)
+background = Label(image=imag_1)
+background.grid(row = 0, column = 0, columnspan = 3)
+#background.place(x=0,y=0, relwidth=1,relheight=1)
+
+def show_map():
+    global background
+    background.grid_forget()
+    background = Label(image=imag_2)
+    background.grid(row = 1, column = 0, columnspan = 3)
+
+    button_back = Button(root,text='Back',padx=117,pady=5,fg='snow',bg='black',command=back)
+    button_back.place(relx=0.5,rely=0.9,anchor=CENTER)
+
+def show_path():
+    global background
+    background.grid_forget()
+    background = Label(image=imag_3)
+    background.grid(row = 1, column = 0, columnspan = 3)
+
+    button_back = Button(root,text='Back',padx=117,pady=5,fg='snow',bg='black',command=back)
+    button_back.place(relx=0.5,rely=0.9,anchor=CENTER)
 
 
-# Chamar o Função botao
-def game():  
-    board = Toplevel()
-    board.title('Gdelivery')
-    board.geometry('800x600')
-    board.resizable(width = False, height = False)
-    board.iconbitmap('assets/delivey.ico')
+def back():
     
-    #ABRE O MAPA
-    imagemTabuleiro = PhotoImage(file = "assets/mapaBrasilia.png")
-    Tabuleiro = Label(board, image=imagemTabuleiro)
-    Tabuleiro.place(x=0,y=0,relwidth=1,relheight=1)
-    
-    #FUNÇAO de movimento
-    def on_move(event):
-        component=event.widget
-        locx, locy = component.winfo_x(), component.winfo_y()
-        w , h =board.winfo_width(),board.winfo_height()
-        mx ,my =component.winfo_width(),component.winfo_height()
-        xpos=(locx+event.x)-(15)
-        ypos=(locy+event.y)-int(my/2)
-        if xpos>=0 and ypos>=0 and w-abs(xpos)>=0 and h-abs(ypos)>=0 and xpos<=w-5 and ypos<=h-5:
-            component.place(x=xpos,y=ypos)
-    
+    global background
+    background.grid_forget()
+    background = Label(image=imag_1)
+    background.grid(row = 0, column = 0, columnspan = 3)
 
-   
-    board.mainloop()
+    butaoNew = Button(root, text='Visualizar Mapa',padx=85,pady=5,fg='snow',bg='black',command = show_map)
+    butaoNew.place(relx=0.5,rely=0.1,anchor=CENTER)
 
-   
+    butaoPvP = Button(root,text='Calcular Rota',padx=90,pady=5,fg='snow',bg='black',command = show_path)
+    butaoPvP.place(relx=0.5,rely=0.2,anchor=CENTER)
+
+    butaoExit = Button(root,text='SAIR',padx=117,pady=5,fg='snow',bg='black',command=root.quit)
+    butaoExit.place(relx=0.5,rely=0.5,anchor=CENTER)
    
 
 # Butões do Menu
-butaoNew = Button(root, text='Adicionar endereço',padx=65,pady=5,fg='snow',bg='black',command = game)
+butaoNew = Button(root, text='Visualizar Mapa',padx=85,pady=5,fg='snow',bg='black',command = show_map)
 butaoNew.place(relx=0.5,rely=0.1,anchor=CENTER)
-butaoPvP = Button(root,text='Mapa rota',padx=90,pady=5,fg='snow',bg='black',command = game)
+
+butaoPvP = Button(root,text='Calcular Rota',padx=90,pady=5,fg='snow',bg='black',command = show_path)
 butaoPvP.place(relx=0.5,rely=0.2,anchor=CENTER)
+
 butaoExit = Button(root,text='SAIR',padx=117,pady=5,fg='snow',bg='black',command=root.quit)
 butaoExit.place(relx=0.5,rely=0.5,anchor=CENTER)
 
